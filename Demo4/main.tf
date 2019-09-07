34d23afe8f8a558c99e2856c6e7ac5de9d43525a
Joey Lorich <joseph@lorich.me>
2019-09-07 17:56:23 -0600

    format tf

--- Demo4/main.tf
+++ Demo4/main.tf
@@ -1,6 +1,6 @@
 module "routetable" {
-  source              = "./demo-module"
-  
+  source = "./demo-module"
+
   resource_group_name = "${var.name}-${var.environment}-rg"
   location            = "westus"
   route_prefixes      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
