070da0c266fadf9517a951ba1d522c1001df1f16
Joey Lorich <joseph@lorich.me>
2019-09-07 17:08:51 -0600

    I can't spell

--- Demo1/main.tf
+++ Demo1/main.tf
@@ -6,7 +6,7 @@
 ##############################################
 
 resource "azurerm_resource_group" "ghost-rg" {
-    name                    = "${var.name}-${var.environemnt}-rg"
+    name                    = "${var.name}-${var.environment}-rg"
     location                = var.loc
     tags                    = var.tags
 }
@@ -15,7 +15,7 @@ resource "azurerm_container_group" "ghost" {
     name                        = "ghost"
     location                    = var.loc
     resource_group_name         = azurerm_resource_group.ghost-rg.name
-    dns_name_label              = "${var.name}-ghost-${var.environemnt}"
+    dns_name_label              = "${var.name}-ghost-${var.environment}"
     ip_address_type             = "public"
     os_type                     = "linux"
     tags                        = var.tags
